c87bfa8230bdc7e787a41f8248e3276b9aa68cb6
Kirill <https://github.com/Khirill3490/tgBotGo>
2026-04-07 11:46:13 +0300

    Для проверки, перед кейклоак

--- auth-module/src/main/java/ru/example/authmodule/service/impl/TransactionServiceImpl.java
+++
@@ -1,6 +0,0 @@
-//package ru.example.authmodule.service.impl;
-//
-//import com.example.all.service.TransactionService;
-//
-//public class TransactionServiceImpl implements TransactionService {
-//}
