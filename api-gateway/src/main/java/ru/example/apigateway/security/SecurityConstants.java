902209533cf34549cf5c6e914400929a92783cbb
Kirill <https://github.com/Khirill3490/tgBotGo>
2026-04-10 12:35:48 +0300

    Отредактирован api. Запускается

--- api-gateway/src/main/java/ru/example/apigateway/security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package ru.example.apigateway.security;
-
-public final class SecurityConstants {
-    public static final String TOKEN_PREFIX = "Bearer ";
-    private SecurityConstants() {
-    }
-}
